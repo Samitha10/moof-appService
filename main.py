a9347f839b281e373ebaa07dff690b2048c4375d
Samitha10 <samithabesinghe1999@gmail.com>
2024-10-08 22:56:39 +0530

    Refactor main.py to add streaming endpoint and remove unused code

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import HTMLResponse, StreamingResponse
+import asyncio
 
 app = FastAPI()
 
 
-@app.get("/home")
-def read_root():
-    return {"Hello": "World"}
+""" @app.get("/streaming")
+async def streaming():
+    async def stream():
+        for i in range(10):
+            yield f"Hello World {i}\n"
+            await asyncio.sleep(1)
+
+    return StreamingResponse(stream(), media_type="text/plain")
+ """
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
